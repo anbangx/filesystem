c2c1c26858eb23dcb12c84964105525d4ac08855
Anbang Xu <anbangx@gmail.com>
2015-01-12 21:10:57 -0800

    add handler in main for lock_server_cache

--- 6.824/lab/lock_smain.cc
+++ 6.824/lab/lock_smain.cc
@@ -31,11 +31,20 @@ main(int argc, char *argv[])
   //jsl_set_debug(2);
 
 #ifndef RSM
+//#define STEP_ONE
+#ifdef STEP_ONE
   lock_server ls;
   rpcs server(atoi(argv[1]), count);
   server.reg(lock_protocol::stat, &ls, &lock_server::stat);
   server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
   server.reg(lock_protocol::release, &ls, &lock_server::release);
+#else
+  lock_server_cache ls;
+  rpcs server(atoi(argv[1]), count);
+  server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
+  server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
+#endif
 #endif
 
 
