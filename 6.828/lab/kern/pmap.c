055ae6abfe19ecaedbcca78ca0e068df462bd2aa
Anbang Xu <anbangx@gmail.com>
2015-02-09 21:54:54 -0800

    temp save

--- 6.828/lab/kern/pmap.c
+++ 6.828/lab/kern/pmap.c
@@ -274,7 +274,13 @@ mem_init_mp(void)
 	//     Permissions: kernel RW, user NONE
 	//
 	// LAB 4: Your code here:
-
+	int i = 0;
+	uint32_t kstacktop_i;
+	for ( ;i < NCPU ;i ++){
+		kstacktop_i = KSTACKTOP - i * (KSTKSIZE + KSTKGAP);
+		boot_map_region(kern_pgdir, kstacktop_i - KSTKSIZE, KSTKSIZE,
+						PADDR(&percpu_kstacks[i]), PTE_W);
+	}
 }
 
 // --------------------------------------------------------------
@@ -642,7 +648,14 @@ mmio_map_region(physaddr_t pa, size_t size)
 	// Hint: The staff solution uses boot_map_region.
 	//
 	// Your code here:
-	panic("mmio_map_region not implemented");
+	void * ret = (void*)base;
+	size = ROUNDUP(size, PGSIZE);
+	if (base + size > MMIOLIM || base + size < base )
+		panic("mmio_map_region : reservation overflow.\n");
+	boot_map_region(kern_pgdir, base, size, pa, PTE_PCD | PTE_PWT | PTE_W);
+	base += size;
+	return ret;
+//	panic("mmio_map_region not implemented");
 }
 
 static uintptr_t user_mem_check_addr;
